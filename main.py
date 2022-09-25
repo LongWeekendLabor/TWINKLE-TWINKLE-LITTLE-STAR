1f95859bb5add35c82b62d4ede0548970e45fb1b
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-09-25 23:01:39 +0800

    refactor: change the architecture of the project

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from lib2to3.pytree import convert
+# import modules
 import pygame
 import os
 
-# define const
-FPS = 60
-WIDTH = 960
-HEIGHT = 540
-GAMENAME = 'UNTITLED'
-
-# define color area
-BLACK = (0, 0, 0)
+# import sources
+from src.const import *
+from src.player import *
 
 # init & create a window
 pygame.init()
@@ -20,43 +15,11 @@ clock = pygame.time.Clock()
 # loading imgs
 # example...
 background_img = pygame.image.load(os.path.join('img', 'sample_bg.png')).convert()
-sample_img = pygame.image.load(os.path.join('img', 'sample.png')).convert()
-
-# sprite
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(sample_img, (38, 38))
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.speed = [8, 8]
-        
-        # absolute position
-        # self.rect.x = 200
-        # self.rect.y = 200
-        
-        # center position
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        
-    def update(self):
-        # get a class that store a list of bool that determine if any key was be clicked
-        key_pressed = pygame.key.get_pressed()
-        
-        # control player to move
-        if key_pressed[pygame.K_RIGHT]: self.rect.x += self.speed[0]
-        if key_pressed[pygame.K_LEFT]: self.rect.x -= self.speed[0]
-        if key_pressed[pygame.K_UP]: self.rect.y -= self.speed[1]
-        if key_pressed[pygame.K_DOWN]: self.rect.y += self.speed[1]
-            
-        # setting border effect
-        if self.rect.right > WIDTH: self.rect.right = WIDTH
-        if self.rect.left < 0: self.rect.left = 0
-        if self.rect.top < 0: self.rect.top = 0
-        if self.rect.bottom > HEIGHT: self.rect.bottom = HEIGHT
+spaceship = pygame.image.load(os.path.join('img', 'sample.png')).convert()
         
 # sprite group
 all_sprites = pygame.sprite.Group()
-player = Player()
+player = Player(spaceship)
 all_sprites.add(player)
     
 # gaming loop
