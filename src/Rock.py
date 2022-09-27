7b08e4d6daee6da0bbb36571d7b2bdfc12222ea6
Kanzaki <92453486+Eric8211@users.noreply.github.com>
2022-09-27 23:25:43 +0800

    feat: add rock rotation

--- src/Rock.py
+++ src/Rock.py
@@ -1,23 +1,87 @@
-from .Const import *
+from src.Const import *
 import pygame
 import random
+import os
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
+        #Down load picture
+        meteorite1_img = pygame.image.load(os.path.join("img","meteorite-1.png")).convert()
+        meteorite2_img = pygame.image.load(os.path.join("img","meteorite-2.png")).convert()
+        meteorite3_img = pygame.image.load(os.path.join("img","meteorite-3.png")).convert()
+        meteorite4_img = pygame.image.load(os.path.join("img","meteorite-4.png")).convert()
+        meteorite_img = [meteorite1_img,meteorite2_img,meteorite3_img,meteorite4_img]
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(COLOR["AZURE"])
+
+        #Using random to determine top or bottom orleft or right
+        GAME_SETUP["RANDOMNUMBER"] = random.randrange(0,4)
+        self.image_ori = meteorite_img[GAME_SETUP["RANDOMNUMBER"]]
+        self.image = self.image_ori.copy()
+        self.image_ori.set_colorkey(COLOR["BLACK"])
         self.rect = self.image.get_rect()
-        self.speedy = random.randrange(1,5)
-        self.speedx = random.randrange(-1,1)
-        self.rect.x = random.randrange(0, GAME_BASE_SETUP["WIDTH"] - self.rect.width)
-        self.rect.y = random.randrange(-100,-40)
-        
-    def update(self):
-        self.rect.y += self.speedy
-        self.rect.x += self.speedx
-        if self.rect.top > GAME_BASE_SETUP["HEIGHT"] or self.rect.right > GAME_BASE_SETUP["WIDTH"] or self.rect.left < 0:
+
+        #Top to bottom
+        if GAME_SETUP["RANDOMNUMBER"] == 0:
             self.speedy = random.randrange(1,5)
             self.speedx = random.randrange(-1,1)
             self.rect.x = random.randrange(0, GAME_BASE_SETUP["WIDTH"] - self.rect.width)
             self.rect.y = random.randrange(-100,-40)
+        #Left to right
+        elif GAME_SETUP["RANDOMNUMBER"] == 1:
+            self.speedx = random.randrange(1,3)
+            self.speedy = random.randrange(-3,3)
+            self.rect.x = random.randrange(-10, -5)
+            self.rect.y = random.randrange(0,GAME_BASE_SETUP["HEIGHT"] - self.rect.height)
+        #Bottom to top
+        elif GAME_SETUP["RANDOMNUMBER"] == 2:
+            self.speedx = random.randrange(-1,1)
+            self.speedy = random.randrange(-3,-1)
+            self.rect.x = random.randrange(0, GAME_BASE_SETUP["WIDTH"] - self.rect.width)
+            self.rect.y = random.randrange(513,530)
+        #Right to left
+        elif GAME_SETUP["RANDOMNUMBER"] == 3:
+            self.speedx = random.randrange(-8,-5)
+            self.speedy = random.randrange(-3,3)
+            self.rect.x = random.randrange(1025, 1030)
+            self.rect.y = random.randrange(0,GAME_BASE_SETUP["HEIGHT"] - self.rect.height)
+        self.total_degree = 0
+        self.rot_degree = random.randrange(-3,3)
+
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree = self.total_degree % 360
+        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        #Top to bottom
+        if self.rect.top > GAME_BASE_SETUP["HEIGHT"] or self.rect.right > GAME_BASE_SETUP["WIDTH"] or self.rect.left < 0 and GAME_SETUP["RANDOMNUMBER"] == 0:
+            self.speedy = random.randrange(1,5)
+            self.speedx = random.randrange(-1,1)
+            self.rect.x = random.randrange(0, GAME_BASE_SETUP["WIDTH"] - self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+        #Left to right
+        elif self.rect.top > GAME_BASE_SETUP["HEIGHT"] or self.rect.left > GAME_BASE_SETUP["WIDTH"] or self.rect.bottom < 0 and  GAME_SETUP["RANDOMNUMBER"] == 1:
+            self.speedx = random.randrange(1,3)
+            self.speedy = random.randrange(-3,3)
+            self.rect.x = random.randrange(-10, -5)
+            self.rect.y = random.randrange(0,GAME_BASE_SETUP["HEIGHT"] - self.rect.height)
+        #Bottom to top
+        elif self.rect.bottom < 0 or self.rect.right > GAME_BASE_SETUP["WIDTH"] or self.rect.left < 0 and GAME_SETUP["RANDOMNUMBER"] == 2:
+            self.speedx = random.randrange(-1,1)
+            self.speedy = random.randrange(-3,-1)
+            self.rect.x = random.randrange(0, GAME_BASE_SETUP["WIDTH"] - self.rect.width)
+            self.rect.y = random.randrange(513,530)
+        #Right to left
+        elif self.rect.top > GAME_BASE_SETUP["HEIGHT"] or self.rect.right < 0 or self.rect.bottom < 0 and GAME_SETUP["RANDOMNUMBER"] == 3:
+            self.speedx = random.randrange(-8,-5)
+            self.speedy = random.randrange(-3,3)
+            self.rect.x = random.randrange(1025, 1030)
+            self.rect.y = random.randrange(0,GAME_BASE_SETUP["HEIGHT"] - self.rect.height)
